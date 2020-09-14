jrohel/libdnf

[microdnf] add command download
Download packages to current directory.

microdnf/commands/download/download.cpp
@@ -1,0 +1,95 @@
+/*
+Copyright (C) 2020 Red Hat, Inc.
+
+This file is part of microdnf: https://github.com/rpm-software-management/libdnf/
+
+Microdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Microdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with microdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "download.hpp"
+
+#include "../../context.hpp"
+
+#include <libdnf/conf/option_string.hpp>
+#include <libdnf/rpm/package.hpp>
+#include <libdnf/rpm/package_set.hpp>
+#include <libdnf/rpm/solv_query.hpp>
+#include <libdnf/rpm/transaction.hpp>
+#include <libsmartcols/libsmartcols.h>
+
+#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+namespace microdnf {
+
+void CmdDownload::set_argument_parser(Context & ctx) {
+    patterns_to_show_options = ctx.arg_parser.add_new_values();
+    auto keys = ctx.arg_parser.add_new_positional_arg(
+        "keys_to_match",
+        ArgumentParser::PositionalArg::UNLIMITED,
+        ctx.arg_parser.add_init_value(std::unique_ptr<libdnf::Option>(new libdnf::OptionString(nullptr))),
+        patterns_to_show_options);
+    keys->set_short_description("List of keys to match");
+
+    auto download = ctx.arg_parser.add_new_command("download");
+    download->set_short_description("download packages to current directory");
+    download->set_description("");
+    download->named_args_help_header = "Optional arguments:";
+    download->positional_args_help_header = "Positional arguments:";
+    download->parse_hook = [this, &ctx](
+                                [[maybe_unused]] ArgumentParser::Argument * arg,
+                                [[maybe_unused]] const char * option,
+                                [[maybe_unused]] int argc,
+                                [[maybe_unused]] const char * const argv[]) {
+        ctx.select_command(this);
+        return true;
+    };
+
+    download->add_positional_arg(keys);
+
+    ctx.arg_parser.get_root_command()->add_command(download);
+}
+
+void CmdDownload::configure([[maybe_unused]] Context & ctx) {}
+
+void CmdDownload::run(Context & ctx) {
+    using LoadFlags = libdnf::rpm::SolvSack::LoadRepoFlags;
+    auto & solv_sack = ctx.base.get_rpm_solv_sack();
+
+    // To search in available repositories (available packages)
+    auto enabled_repos = ctx.base.get_rpm_repo_sack().new_query().ifilter_enabled(true);
+    for (auto & repo : enabled_repos.get_data()) {
+        ctx.load_rpm_repo(*repo.get());
+    }
+
+    for (auto & repo : enabled_repos.get_data()) {
+        solv_sack.load_repo(
+            *repo.get(),
+            LoadFlags::USE_FILELISTS | LoadFlags::USE_PRESTO | LoadFlags::USE_UPDATEINFO | LoadFlags::USE_OTHER);
+    }
+
+    libdnf::rpm::PackageSet result_pset(&solv_sack);
+    libdnf::rpm::SolvQuery full_solv_query(&solv_sack);
+    for (auto & pattern : *patterns_to_show_options) {
+        libdnf::rpm::SolvQuery solv_query(full_solv_query);
+        solv_query.resolve_pkg_spec(dynamic_cast<libdnf::OptionString *>(pattern.get())->get_value(), true, true, true, true, true, {});
+        result_pset |= solv_query.get_package_set();
+    }
+
+    download_packages(result_pset, ".");
+}
+
+}  // namespace microdnf

microdnf/commands/download/download.hpp
@@ -1,0 +1,44 @@
+/*
+Copyright (C) 2020 Red Hat, Inc.
+
+This file is part of microdnf: https://github.com/rpm-software-management/libdnf/
+
+Microdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Microdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with microdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef MICRODNF_COMMANDS_DOWNLOAD_DOWNLOAD_HPP
+#define MICRODNF_COMMANDS_DOWNLOAD_DOWNLOAD_HPP
+
+#include "../command.hpp"
+
+#include <libdnf/conf/option_bool.hpp>
+
+#include <memory>
+#include <vector>
+
+namespace microdnf {
+
+class CmdDownload : public Command {
+public:
+    void set_argument_parser(Context & ctx) override;
+    void configure(Context & ctx) override;
+    void run(Context & ctx) override;
+
+private:
+    std::vector<std::unique_ptr<libdnf::Option>> * patterns_to_show_options{nullptr};
+};
+
+}  // namespace microdnf
+
+#endif

redpesk-labs/red-microdnf

commands/download/download.cpp
@@ -1,0 +1,96 @@
+/*
+Copyright (C) 2020 Red Hat, Inc.
+
+This file is part of microdnf: https://github.com/rpm-software-management/libdnf/
+
+Microdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Microdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with microdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "download.hpp"
+
+#include "../../context.hpp"
+
+#include <libdnf/conf/option_string.hpp>
+#include <libdnf/rpm/package.hpp>
+#include <libdnf/rpm/package_set.hpp>
+#include <libdnf/rpm/solv_query.hpp>
+#include <libdnf/rpm/transaction.hpp>
+#include <libsmartcols/libsmartcols.h>
+
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+namespace microdnf {
+
+void CmdDownload::set_argument_parser(Context & ctx) {
+    patterns_to_download_options = ctx.arg_parser.add_new_values();
+    auto keys = ctx.arg_parser.add_new_positional_arg(
+        "keys_to_match",
+        ArgumentParser::PositionalArg::UNLIMITED,
+        ctx.arg_parser.add_init_value(std::unique_ptr<libdnf::Option>(new libdnf::OptionString(nullptr))),
+        patterns_to_download_options);
+    keys->set_short_description("List of keys to match");
+
+    auto download = ctx.arg_parser.add_new_command("download");
+    download->set_short_description("download packages to current directory");
+    download->set_description("");
+    download->named_args_help_header = "Optional arguments:";
+    download->positional_args_help_header = "Positional arguments:";
+    download->parse_hook = [this, &ctx](
+                               [[maybe_unused]] ArgumentParser::Argument * arg,
+                               [[maybe_unused]] const char * option,
+                               [[maybe_unused]] int argc,
+                               [[maybe_unused]] const char * const argv[]) {
+        ctx.select_command(this);
+        return true;
+    };
+
+    download->add_positional_arg(keys);
+
+    ctx.arg_parser.get_root_command()->add_command(download);
+}
+
+void CmdDownload::configure([[maybe_unused]] Context & ctx) {}
+
+void CmdDownload::run(Context & ctx) {
+    using LoadFlags = libdnf::rpm::SolvSack::LoadRepoFlags;
+    auto & solv_sack = ctx.base.get_rpm_solv_sack();
+
+    // To search in available repositories (available packages)
+    auto enabled_repos = ctx.base.get_rpm_repo_sack().new_query().ifilter_enabled(true);
+    for (auto & repo : enabled_repos.get_data()) {
+        ctx.load_rpm_repo(*repo.get());
+    }
+
+    for (auto & repo : enabled_repos.get_data()) {
+        solv_sack.load_repo(
+            *repo.get(),
+            LoadFlags::USE_FILELISTS | LoadFlags::USE_PRESTO | LoadFlags::USE_UPDATEINFO | LoadFlags::USE_OTHER);
+    }
+
+    libdnf::rpm::PackageSet result_pset(&solv_sack);
+    libdnf::rpm::SolvQuery full_solv_query(&solv_sack);
+    for (auto & pattern : *patterns_to_download_options) {
+        libdnf::rpm::SolvQuery solv_query(full_solv_query);
+        solv_query.resolve_pkg_spec(
+            dynamic_cast<libdnf::OptionString *>(pattern.get())->get_value(), true, true, true, true, true, {});
+        result_pset |= solv_query.get_package_set();
+    }
+
+    download_packages(result_pset, ".");
+}
+
+}  // namespace microdnf

commands/download/download.hpp
@@ -1,0 +1,44 @@
+/*
+Copyright (C) 2020 Red Hat, Inc.
+
+This file is part of microdnf: https://github.com/rpm-software-management/libdnf/
+
+Microdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Microdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with microdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef MICRODNF_COMMANDS_DOWNLOAD_DOWNLOAD_HPP
+#define MICRODNF_COMMANDS_DOWNLOAD_DOWNLOAD_HPP
+
+#include "../command.hpp"
+
+#include <libdnf/conf/option_bool.hpp>
+
+#include <memory>
+#include <vector>
+
+namespace microdnf {
+
+class CmdDownload : public Command {
+public:
+    void set_argument_parser(Context & ctx) override;
+    void configure(Context & ctx) override;
+    void run(Context & ctx) override;
+
+private:
+    std::vector<std::unique_ptr<libdnf::Option>> * patterns_to_download_options{nullptr};
+};
+
+}  // namespace microdnf
+
+#endif